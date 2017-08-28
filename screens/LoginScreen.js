d24f1b4d599763a9d1c853fd2253dadf7440839d
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2017-08-28 15:18:52 +0200

    init + more details

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,13 +6,35 @@ import React, {Component} from 'react';
 
 import {
     Text,
+    TextInput,
+    View,
 } from 'react-native';
+
+import NavBar from 'react-native-navbar';
+
+
+
 
 
 export default class LoginScreen extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {nickname: ""};
+
+
+
+        //Getters
+        this.getState = () => {
+            return this.state;
+        };
+
+        this.getNickname = () => {
+            return this.state.nickname
+        };
+
+
+        //Setters
 
     };
 
@@ -24,10 +46,28 @@ export default class LoginScreen extends Component {
 
 
     render() {
+
+        const screenConfigNavbar = {
+            title: 'Login',
+        };
+
+        const screenConfigNavbarButtonLeft = {
+            title: 'Accueil',
+            handler: () => this.props.navigation.navigate('HomeScreen'),
+        };
+
         return (
-            <Text>
-                Ma page Login
-            </Text>
+            <View>
+                <NavBar title={screenConfigNavbar} leftButton={screenConfigNavbarButtonLeft} />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(nickname) => this.setState({nickname})}
+                    value={this.state.nickname}
+                />
+                <Text>
+                    Votre pseudo : {this.getNickname()}
+                </Text>
+            </View>
         );
     };
 
